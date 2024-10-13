34473427fe98c2f0603e6df6b94ce0be14108bac
Milad Ghahremani <139281846+MGh-97@users.noreply.github.com>
2024-10-13 10:30:29 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 st.set_page_config(page_title="My App", page_icon="logo.JPG")
